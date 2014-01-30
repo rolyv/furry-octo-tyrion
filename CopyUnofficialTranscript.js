5c359f1d0e6ccade11593cc9cde4a80766cbe5b7
Roly <rolyv19@gmail.com>
2014-01-30 06:14:42 -0500

    Updated js to wait for elements to exist

--- CopyUnofficialTranscript.js
+++ CopyUnofficialTranscript.js
@@ -5,47 +5,57 @@ function fakeClick(event, anchorObj) {
     if(event.target !== anchorObj) {
       var evt = document.createEvent("MouseEvents"); 
       evt.initMouseEvent("click", true, true, window, 
-          0, 0, 0, 0, 0, false, false, false, false, 0, null); 
+	  0, 0, 0, 0, 0, false, false, false, false, 0, null); 
       var allowDefault = anchorObj.dispatchEvent(evt);
-      // you can check allowDefault for false to see if
-      // any handler called evt.preventDefault().
-      // Firefox will *not* redirect to anchorObj.href
-      // for you. However every other browser will.
     }
   }
 }
 
 function selectElementContents(el) {
-    var body = document.body, range, sel;
-    if (document.createRange && window.getSelection) {
-        range = document.createRange();
-        sel = window.getSelection();
-        sel.removeAllRanges();
-        try {
-            range.selectNodeContents(el);
-            sel.addRange(range);
-        } catch (e) {
-            range.selectNode(el);
-            sel.addRange(range);
-        }
-    } else if (body.createTextRange) {
-        range = body.createTextRange();
-        range.moveToElementText(el);
-        range.select();
+  var body = document.body, range, sel;
+  if (document.createRange && window.getSelection) {
+    range = document.createRange();
+    sel = window.getSelection();
+    sel.removeAllRanges();
+    try {
+      range.selectNodeContents(el);
+      sel.addRange(range);
+    } catch (e) {
+      range.selectNode(el);
+      sel.addRange(range);
     }
+  } else if (body.createTextRange) {
+    range = body.createTextRange();
+    range.moveToElementText(el);
+    range.select();
+  }
 }
 
-var mainMenu = document.getElementById("pthnavbca_PORTAL_ROOT_OBJECT");
+function waitForElement(el, isFrame){
+  var elem = null;
+  
+  while (!elem) {
+    if (isFrame) {  
+      elem = window.frames[0].document.getElementById(el);
+    }
+    else {
+      elem = document.getElementById(el);
+    }
+  }
+}
+
+var mainMenu = waitForElement("pthnavbca_PORTAL_ROOT_OBJECT", false);
 fakeClick("click", mainMenu);
-var selfService = document.getElementById("fldra_CO_EMPLOYEE_SELF_SERVICE");
+var selfService = waitForElement("fldra_CO_EMPLOYEE_SELF_SERVICE", false);
 fakeClick("click", selfService);
-var academicRecords = document.getElementById("fldra_HCCC_ACADEMIC_RECORDS");
+var academicRecords = waitForElement("fldra_HCCC_ACADEMIC_RECORDS", false);
 fakeClick("click", academicRecords);
-var unofficialTranscript = document.getElementById("crefli_HC_SS_AA_REPORT1_GBL");
+var unofficialTranscript = waitForElement("crefli_HC_SS_AA_REPORT1_GBL", false);
 fakeClick("click", unofficialTranscript);
-window.frames[0].document.getElementById("DERIVED_AA2_TSCRPT_TYPE3").options[3].selected = true;
-var go = window.frames[0].document.getElementById("GO");
+var transcriptSelect = waitForElement("DERIVED_AA2_TSCRPT_TYPE3", true);
+transcriptSelect.options[3].selected = true;
+var go = waitForElement("GO", true);
 fakeClick("click", go);
 
-var text = window.frames[0].document.getElementById("ACE_$ICField5$0");
+var text = waitForElement("ACE_$ICField5$0", true);
 selectElementContents(text);
